b3b4b8d6c5110a62b4e0c71e2489339a24f5e5f7
rchitac <chitacrazvan@gmail.com>
2019-10-19 11:18:46 +0300

    Hibernate with Database updated

---
+++ src/main/java/db/Dbinitializer.java
@@ -0,0 +1,34 @@
+package db;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public abstract class Dbinitializer {
+
+    protected SessionFactory sessionFactory;
+    protected Session session;
+    protected Transaction transaction;
+
+    public Dbinitializer() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        MetadataSources sources = new MetadataSources(registry);
+        Metadata metadata = sources.getMetadataBuilder().build();
+        sessionFactory = metadata.getSessionFactoryBuilder().build();
+    }
+
+    protected void openSessionAndTransaction(){
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+    }
+
+    protected void closeSessionAndTransaction(){
+        transaction.commit();
+        session.close();
+    }
+
+}
