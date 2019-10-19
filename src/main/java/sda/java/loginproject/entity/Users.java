b026d11d1c5f44e380f712f8a01add6630866d3c
Camelia-code <camelia_camy48@yahoo.com>
2019-10-19 11:16:45 +0300

    Pas 2 entitatea Users

---
+++ src/main/java/sda/java/loginproject/entity/Users.java
@@ -0,0 +1,88 @@
+package sda.java.loginproject.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Users")
+public class Users {
+    private static final String USERS_SEQUENCE = "users_id_seq";
+    private static final String USERS_GENERATOR = "student_generator";
+
+
+    @Id
+    @SequenceGenerator(name = USERS_GENERATOR, sequenceName = USERS_SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = USERS_GENERATOR)
+
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    public Users() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return id == users.id &&
+                Objects.equals(username, users.username) &&
+                Objects.equals(password, users.password) &&
+                Objects.equals(role, users.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
